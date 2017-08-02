1a3edf398921c37554c64a8ca317bff559465353
monmee <jazzmonmee@gmail.com>
2017-08-03 00:54:14 +0900

    letとlet\!を使い分けよう2

--- spec/lib/calc_spec.rb
+++ spec/lib/calc_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Calc do
   # let
   # exampleごとに結果がキャッシュされる
   # 遅延評価
-  let(:tax) { calc.tax = 0.05 }
+  let!(:tax) { calc.tax = 0.05 }
   it { expect(tax).to eq 0.05 }
   it { expect(calc.price(100)).to eq 105 }
 end
