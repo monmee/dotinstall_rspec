dc1c3d8faf808eb0d222be5d1e09d0c1c0f1639d
monmee <jazzmonmee@gmail.com>
2017-08-03 00:17:52 +0900

    開発サイクルを進めよう

--- spec/lib/calc_spec.rb
+++ spec/lib/calc_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe 'A calc' do
     calc = Calc.new
     expect(calc.add(2,3)).to eq 5
   end
+  it 'given 5 and 8, returns 13' do
+    calc = Calc.new
+    expect(calc.add(5,8)).to eq 13
+  end
 end
