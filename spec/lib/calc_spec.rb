33c8d6b30756dd814c17142119922119f1281709
monmee <jazzmonmee@gmail.com>
2017-08-03 00:21:45 +0900

    describe,contextを使おう

--- spec/lib/calc_spec.rb
+++ spec/lib/calc_spec.rb
@@ -1,14 +1,15 @@
 require 'calc'
 
 RSpec.describe 'A calc' do
-  before do
-    @calc = Calc.new
+  describe 'when normal mode' do
+    it 'given 2 and 3, returns 5' do
+      calc = Calc.new
+      expect(calc.add(2,3)).to eq 5
+    end
   end
 
-  it 'given 2 and 3, returns 5' do
-    expect(@calc.add(2,3)).to eq 5
+  describe 'when graph mode' do
+    it 'draws graph'
   end
-  it 'given 5 and 8, returns 13' do
-    expect(@calc.add(5,8)).to eq 13
-  end
+
 end
