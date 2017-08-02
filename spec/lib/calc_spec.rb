ed556d6757dc54b9955a8e560dd21ed7ade44620
monmee <jazzmonmee@gmail.com>
2017-08-03 00:26:23 +0900

    exampleを使ってみよう

--- spec/lib/calc_spec.rb
+++ spec/lib/calc_spec.rb
@@ -1,16 +1,9 @@
 require 'calc'
 
-RSpec.describe 'A calc' do
-  context 'when normal mode' do
-    it 'given 2 and 3, returns 5' do
-      calc = Calc.new
-      expect(calc.add(2,3)).to eq 5
-    end
-  end
-
-  context 'when graph mode' do
-    it 'draws graph'
-  end
+RSpec.describe Calc do
+  # example/specify
+  it {
+    calc = Calc.new
+    expect(calc.add(2,3)).to eq 5
+  }
 end
-
-# describeはモノに対して、contextは条件に対して使われるのが推奨。
