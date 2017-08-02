c3fc3c64d834cd084963abd84b2c66cf83e9fc3c
monmee <jazzmonmee@gmail.com>
2017-08-03 01:10:07 +0900

    shared_examplesを使おう

--- spec/lib/calc_spec.rb
+++ spec/lib/calc_spec.rb
@@ -1,12 +1,19 @@
 require 'calc'
 
-# test double: 代役オブジェクト
-# message expectation: 呼ばれなかったらテストが失敗する
+RSpec.shared_examples 'basic functions' do
+  it 'can add'
+  it 'can subtrace'
+  it 'can multiply'
+  it 'can devide'
+end
+
 RSpec.describe Calc do
-  it {
-    logger = double('logger')
-    expect(logger).to receive(:log)
-    calc = Calc.new(logger)
-    expect(calc.add(5, 2)).to eq 7
-  }
+  context 'when normal mode' do
+    include_examples 'basic functions'
+  end
+
+  context 'when expert mode' do
+    include_examples 'basic functions'
+    it 'draws graph'
+  end
 end
