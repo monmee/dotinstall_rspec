47c3670e8e2b04dfd46390fa7a420c694023a8e0
monmee <jazzmonmee@gmail.com>
2017-08-03 00:38:23 +0900

    subjectを使ってみよう

--- spec/lib/calc_spec.rb
+++ spec/lib/calc_spec.rb
@@ -1,18 +1,8 @@
 require 'calc'
 
 RSpec.describe Calc do
-  # example/specify
+  subject(:calc) { Calc.new }
   it {
-    calc = Calc.new
     expect(calc.add(2,3)).to eq 5
-    expect(calc.add(2,3)).not_to eq 5
-    expect(calc.add(2,3)).to be true
-    expect(calc.add(2,3)).to be false
-    expect(calc.add(2,3)).to be > 5
-    expect(calc.add(2,3)).to be < 5
-    expect(calc.add(2,3)).to be_between(1, 10).inclusive
-    expect(calc.add(2,3)).to respond_to(:add)
-    expect(calc.add(2,3)).to be_integer
   }
-  it 'draws graph'
 end
