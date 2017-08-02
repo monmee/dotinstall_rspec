915df53165a3c1b487c372c2d29b0f188fbda6fa
monmee <jazzmonmee@gmail.com>
2017-08-03 00:30:46 +0900

    matcherを使ってみよう

--- spec/lib/calc_spec.rb
+++ spec/lib/calc_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe Calc do
   it {
     calc = Calc.new
     expect(calc.add(2,3)).to eq 5
+    expect(calc.add(2,3)).not_to eq 5
+    expect(calc.add(2,3)).to be true
+    expect(calc.add(2,3)).to be false
+    expect(calc.add(2,3)).to be > 5
+    expect(calc.add(2,3)).to be < 5
+    expect(calc.add(2,3)).to be_between(1, 10).inclusive
+    expect(calc.add(2,3)).to respond_to(:add)
+    expect(calc.add(2,3)).to be_integer
   }
   it 'draws graph'
 end
